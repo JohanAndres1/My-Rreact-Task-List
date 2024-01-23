46286fdc97fce91f39c537b3acbf942969328899
Johan Sierra <johansierra553@gmail.com>
2024-01-22 22:13:50 -0500

    Refactor App component to use custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,11 @@
-import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import TaskAdd from './components/TaskAdd'
 import TaskList from './components/TaskList'
+import useTaskManager from './hooks/useTaskManager'
 import './App.css'
 
 function App() {
-  const [tasksItems, setTasksItems] = useState([])
-  const [taskId, setTaskId] = useState(1)
-
-  function createNewTask(taskName, taskDescription) {
-
-    if (!tasksItems.find(task => task.title === taskName)) {
-      setTasksItems([...tasksItems, { title: taskName, completed: false, id: taskId, description: taskDescription }])
-      setTaskId(taskId + 1)
-    } else {
-      alert('La tarea ya existe')
-    }
-  }
-
-  function toggleCompleted(task) {
-    setTasksItems(
-      tasksItems.map(t => 
-        t.title === task.title ? {...t, completed : !t.completed} : t
-      )
-    )
-  }
-
-  const deleteTask = (title) => {
-    const newTask = [...tasksItems]
-    const taskIndex = newTask.findIndex((task) => task.title === title)
-    newTask.splice(taskIndex, 1)
-    setTasksItems(newTask)
-  }
-
-  const editTask = (oldTitle, newTitle, description) => {
-    setTasksItems(
-      tasksItems.map(task => 
-        task.title === oldTitle ? {...task, title: newTitle, description: description} : task
-      )
-    )
-  }
-
-  useEffect(() => {
-    const data = localStorage.getItem("tasks")
-    if (data) {
-      setTasksItems(JSON.parse(data))
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasksItems))
-  }, [tasksItems])
-  
+  const { tasksItems, createNewTask, toggleCompleted, deleteTask, editTask } = useTaskManager()
 
   return (
     <>
